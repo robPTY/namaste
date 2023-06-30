bb0f00decb2ee3e9f9c337541c8a39f8a6a14855
robPTY <rcadown8@gmail.com>
2023-06-29 22:40:36 -0500

    fixed and styled header

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,11 +7,11 @@ function Home() {
     return (
         <div>
             <header id="main_header">
-                <h1>NAMAST3</h1>
+                <h1 id="main-logo-header">namaste.</h1>
                 <nav>
                     <ul>
-                        <li><Link to="/guided-meditation">Guided Meditation</Link></li>
-                        <li><Link to="/mindfulness-exercises">Mindfulness Exercises</Link></li>
+                        <li><Link to="/guided-meditation">Meditation</Link></li>
+                        <li><Link to="/mindfulness-exercises">Exercises</Link></li>
                         <li><Link to="/community">Community</Link></li>
                         <li><Link to="/profile">Profile</Link></li>
                     </ul>
@@ -23,7 +23,6 @@ function Home() {
                 <p>Discover a calmer, more centered you with our guided meditation sessions and mindfulness exercises.</p>
                 <Link to="/signup">Join Now</Link>
             </section>
-
         </div>
     );
 }

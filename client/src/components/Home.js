9416866f939a660c7f7e4fc8782c18c4c53ad653
robPTY <rcadown8@gmail.com>
2023-07-03 22:27:37 -0500

    fixd home page w/ temporary layout

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,54 +5,44 @@ import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <div className="container">
-            <header>
-                <h1>Welcome to Our Website</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id libero in lorem facilisis pellentesque. Proin semper rutrum tellus eu consequat. Sed fermentum, dui eget tempor iaculis, nulla felis semper turpis, sit amet pharetra mauris felis sit amet enim.</p>
-                <a href="#about">Learn More</a>
-            </header>
-            <section id="about">
-                <div className="about-content">
-                    <h2>About Us</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id libero in lorem facilisis pellentesque. Proin semper rutrum tellus eu consequat. Sed fermentum, dui eget tempor iaculis, nulla felis semper turpis, sit amet pharetra mauris felis sit amet enim.</p>
-                </div>
-                <div className="about-image">
-                    <img src="/path/to/image" alt="About" />
-                </div>
-            </section>
-            <section id="features">
-                <h2>Key Features</h2>
-                <div className="feature-card">
-                    <img src="/path/to/feature-image" alt="Feature" />
-                    <h3>Feature 1</h3>
+        <div className="home-container">
+            <div className="hero-section">
+                <h1>Welcome to My Website!</h1>
+                <p>This is the homepage of my website.</p>
+                <Link to="/about" className="btn">Learn More</Link>
+            </div>
+            <section className="features-section">
+                <div className="feature">
+                    <img src="feature1.png" alt="Feature 1" />
+                    <h2>Feature 1</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
-                <div className="feature-card">
-                    <img src="/path/to/feature-image" alt="Feature" />
-                    <h3>Feature 2</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div className="feature">
+                    <img src="feature2.png" alt="Feature 2" />
+                    <h2>Feature 2</h2>
+                    <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
-                <div className="feature-card">
-                    <img src="/path/to/feature-image" alt="Feature" />
-                    <h3>Feature 3</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-            </section>
-            <section id="testimonial">
-                <h2>Testimonials</h2>
-                <div className="testimonial-card">
-                    <img src="/path/to/user-image" alt="User" />
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id libero in lorem facilisis pellentesque. Proin semper rutrum tellus eu consequat."</p>
-                    <h4>- John Doe</h4>
-                </div>
-                <div className="testimonial-card">
-                    <img src="/path/to/user-image" alt="User" />
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id libero in lorem facilisis pellentesque. Proin semper rutrum tellus eu consequat."</p>
-                    <h4>- Jane Smith</h4>
+                <div className="feature">
+                    <img src="feature3.png" alt="Feature 3" />
+                    <h2>Feature 3</h2>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
             </section>
-            <footer>
-                <p>&copy; 2023 Your Website. All rights reserved.</p>
+            <section className="testimonial-section">
+                <h2>What People Say</h2>
+                <div className="testimonial">
+                    <img src="avatar1.png" alt="Avatar 1" />
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</p>
+                    <p className="testimonial-author">- John Doe</p>
+                </div>
+                <div className="testimonial">
+                    <img src="avatar2.png" alt="Avatar 2" />
+                    <p>"Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                    <p className="testimonial-author">- Jane Smith</p>
+                </div>
+            </section>
+            <footer className="footer">
+                <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
             </footer>
         </div>
     );
